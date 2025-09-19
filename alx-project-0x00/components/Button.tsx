8dc56eb26ed039df0e58d53bbf65d12fdfa29c4f
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-19 10:45:07 +0100

    created button

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,10 +1,10 @@
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, className, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 transition ${className}`}
+      className={`px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 transition ${styles}`}
     >
       {title}
     </button>
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,9 +6,9 @@ const Landing: React.FC = () => {
         <div>
             <h1 className=" text-xl font-extralight ">Landing Page</h1>
             <Card />
-            <Button title="Click Me" className="size-1/4 rounded-sm" onClick={() => alert('Button Clicked!')} />
-            <Button title="Click Me" className="size-2/4 rounded-md" onClick={() => alert('Button Clicked!')} />
-            <Button title="Click Me" className="size-3/4 rounded-full" onClick={() => alert('Button Clicked!')} />
+            <Button title="Click Me" styles="size-1/4 rounded-sm" onClick={() => alert('Button Clicked!')} />
+            <Button title="Click Me" styles="size-2/4 rounded-md" onClick={() => alert('Button Clicked!')} />
+            <Button title="Click Me" styles="size-3/4 rounded-full" onClick={() => alert('Button Clicked!')} />
         </div>
     )
 }
